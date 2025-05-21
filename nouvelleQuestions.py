d9f5446932662a2e30f9da41cf0efc335912bc30
Nope-Nieks <160030142+Nope-Nieks@users.noreply.github.com>
2025-05-21 17:15:43 +0200

    Presque

--- nouvelleQuestions.py
+++ nouvelleQuestions.py
@@ -32,7 +32,7 @@ def parse_questions_and_answers(input_text):
 # Texte contenant les questions et les réponses
 input_text = """
 Q1) En matière de régression linéaire, laquelle des alternatives suivantes ne comprend que des
-synonymes de ‘variable dépendante’ ?
+synonymes de 'variable dépendante' ?
 (i) Le régressant
 (ii) Le régresseur
 (iii) La variable expliquée
@@ -44,11 +44,11 @@ synonymes de ‘variable dépendante’ ?
 (i), (ii), (iii) et (v).
 
 Q2) En matière de régression linéaire, laquelle des alternatives suivantes ne comprend que des
-synonymes de ‘variable indépendante’ ?
+synonymes de 'variable indépendante' ?
 (i) Le régresseur
 (ii) La variable de réponse=
 (iii) La variable causale
-(iv) La variable d’effet
+(iv) La variable d'effet
 (ii) et (iv) seulement.
 (i) et (iii) seulement.
 (i), (ii) et (iii) seulement.
@@ -57,9 +57,9 @@ synonymes de ‘variable indépendante’ ?
 Q3) Laquelle des propositions suivantes est vraie concernant le modèle classique de régression?
 y a une distribution de probabilité.
 x a une distribution de probabilité.
-Le terme d’erreur peut être corrélé avec x.
-Pour qu’un modèle soit adéquat, l’erreur estimée (e) doit être nulle pour chaque donnée observée.
-Q4) Laquelle des propositions suivantes est vraie concernant l’estimation par MCO?
+Le terme d'erreur peut être corrélé avec x.
+Pour qu'un modèle soit adéquat, l'erreur estimée (e) doit être nulle pour chaque donnée observée.
+Q4) Laquelle des propositions suivantes est vraie concernant l'estimation par MCO?
 MCO minimise la somme des distances verticales entre les données observées et la droite de
 régression.
 MCO minimise la somme des distances horizontales entres données observées et la droite de
@@ -70,20 +70,4 @@ MCO minimise la somme des carrés des distances verticales entre les données ob
 droite de régression.
 """
 
-questions_and_answers = splitQuestions(input_text)
-
-questions = []
-for question_text in questions_and_answers:
-    questions.append(parse_questions_and_answers(question_text[1]))
-
-# Demander à l'utilisateur de fournir le numéro de la bonne réponse
-for i, question in enumerate(questions, 1):
-    print(question['question'])
-    for j, answer in enumerate(question['answers'], 1):
-        print(f"{j}. {answer['text']}")
-    user_input = int(input("Entrez le numéro de la bonne réponse : "))
-    question['answers'][user_input - 1]['correct'] = True
-
-# Écrire les questions dans un fichier JSON
-with open("questions.json", "w") as file:
-    json.dump(questions, file, indent=4)
+j
