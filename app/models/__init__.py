1b6a8d2ff4cd7b068179554a285f0e6851907eda
Nope-Nieks <160030142+Nope-Nieks@users.noreply.github.com>
2025-05-21 15:17:37 +0200

    ok

--- app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.models.user import User
-from app.models.quiz import Quiz, Question, Answer
-from app.models.score import Score
-
-__all__ = ['User', 'Quiz', 'Question', 'Answer', 'Score'] 
